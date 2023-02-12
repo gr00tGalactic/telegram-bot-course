b7b9b2cb0db07d2466efa5d4364ee4efd403a548
gr00tGalactik <gr00tgalactik@gmail.com>
2023-02-13 04:17:49 +0500

    init

---
+++ index.js
@@ -0,0 +1,59 @@
+const TelegramApi = require('node-telegram-bot-api')
+const {gameOptions, againOptions} = require('./options')
+const token = '6187369321:AAHVnSttwtuauVEwmvP_85iPl8TgQIAeje8'
+
+const bot = new TelegramApi(token, {polling: true})
+
+const chats = {}
+
+
+
+const startGame = async (chatId) => {
+    await bot.sendMessage(chatId, `Сейчас я загадаю цифру от 0 до 9, а ты должен её угадать!`);
+    const randomNumber = Math.floor(Math.random() * 10);
+    chats[chatId] = randomNumber;
+    await bot.sendMessage(chatId, 'Отгадывай', gameOptions);
+}
+
+const start = () => {
+    bot.setMyCommands([
+        {command: '/start', description: 'Начальное приветствие'},
+        {command: '/info', description: 'Получить информацию о пользователе'},
+        {command: '/game', description: 'Игра угадай цифру'},
+    ])
+
+    bot.on('message', async msg => {
+        const text = msg.text;                              // Сообщение которое отправил пользователь
+        const chatId = msg.chat.id;                         // id чата, с пользователем, отправившим сообщение
+        // bot.sendMessage(chatId, `Ты написал мне ${text}`)   // Отправка сообщения пользователю в ответ на его сообщение
+    
+        if (text === '/start') {                    // Эта команда срабатывает как только пользователь нажал снизу кномку "Запустить"
+            await bot.sendSticker(chatId, 'https://tlgrm.eu/_/stickers/ea5/382/ea53826d-c192-376a-b766-e5abc535f1c9/7.jpg');
+            return bot.sendMessage(chatId, `Добро пожаловать в телеграм бот telegram-bot-course`);
+        }
+        if (text === '/info') {
+            return bot.sendMessage(chatId, `Тебя зовут ${msg.from.first_name} ${msg.from.last_name}`);
+        }
+        if (text === '/game') {
+            return startGame(chatId);
+        }
+        return bot.sendMessage(chatId, 'Я тебя не понимаю, попробуй ещё раз!)');
+    })
+
+    bot.on('callback_query', async msg => {
+        const data = msg.data;
+        const chatId = msg.message.chat.id;
+        if (data === '/again') {
+            return startGame(chatId)
+        }
+        if (data === chats[chatId]) {
+            return await bot.sendMessage(chatId, `Поздравляю, ты отгадал цифру ${chats[chatId]}`, againOptions)
+        } else {
+            return bot.sendMessage(chatId, `К сожалению ты не угадал, бот загадал цифру ${chats[chatId]}`, againOptions)
+        }
+    })
+    
+}
+
+
+start();
